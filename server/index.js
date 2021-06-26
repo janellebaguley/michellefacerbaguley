4ba8a2f55ea1fb30d3723ba5e611ecb0387776f8
janellebaguley <nellie.baguley@gmail.com>
2021-06-26 07:37:42 -0600

    controller and index updates

--- server/index.js
+++ server/index.js
@@ -1,28 +1,12 @@
 require('dotenv').config({path: __dirname + '/../.env'})
 const express = require('express'),
-      ctrl = require('./controllers/controller'),
-      massive = require('massive'),
-      session = require('express-session');
+      ctrl = require('./controllers/controller')
 const path = require('path')
-const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+const {SERVER_PORT} = process.env
 
 const app = express()
 app.use(express.json())
-app.use(session({
-    resave: false,
-    saveUninitialized: true,
-    secret: SESSION_SECRET,
-    cookie: {maxAge: 1000 * 60 * 60 *24 * 365}
-}));
-
-massive({
-    connectionString: CONNECTION_STRING,
-    ssl: {rejectUnauthorized: false}
-})
-.then((db) => {
-    app.set('db', db)
-    console.log('db connected')
-})
+ 
 
 app.use(express.static(__dirname + '/../build'))
 
